0f5a03bf04b0ec44885206dbe55196cf90b959dd
Berselis Mendoza <berselismendoza@gmail.com>
2022-07-11 23:37:46 -0400

    Deploy
    ultimos arreglos

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,35 @@
-/*!
-* Start Bootstrap - Shop Homepage v5.0.5 (https://startbootstrap.com/template/shop-homepage)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+
+const arregloItems = [];
+
+
+
+arregloItems.forEach(item => {
+
+    const div = document.createAttribute('div');
+    div.className = 'col mb-5';
+
+    div.innerHtml = `
+                    <div class="card h-100">
+                            <div id="ItemCant" class="item-cant">99</div>
+                            <!-- Product image-->
+                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                        
+
+                            <!-- Product details-->
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <!-- Product name-->
+                                    <h5 class="fw-bolder">Fancy Product</h5>
+                                    <!-- Product price-->
+                                    $40.00 - $80.00
+                                </div>
+                            </div>
+                            <!-- Product actions-->
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">View options</a></div>
+                            </div>
+                        </div>`;
+
+});
+
+
