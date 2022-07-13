ff32d9655eac7e6464b18fec1f61916f333e53b6
Berselis Mendoza <berselismendoza@gmail.com>
2022-07-12 23:19:05 -0400

    Deploy
    -Agregando al carito

--- js/main.js
+++ js/main.js
@@ -1,35 +1,35 @@
 const articulos = [
     {
         "Id":"1",
-        "Name":"laptop",
+        "Name":"laptop Lenovo",
         "Price": 500,
         "ImgRoot": "https://es.digitaltrends.com/wp-content/uploads/2022/06/surface-laptop-go-2-feat.jpg?resize=625%2C417&p=1",
         "Stock": 10
     },
     {
         "Id":"2",
-        "Name":"Mouse",
+        "Name":"Mouse Corsair",
         "Price": 200,
         "ImgRoot": "https://thermaltake.azureedge.net/pub/media/catalog/product/cache/6bf0ed99c663954fafc930039201ed07/l/2/l20m01.jpg",
         "Stock": 10
     },
     {
         "Id":"3",
-        "Name":"Teclado",
+        "Name":"Teclado HP",
         "Price": 500,
         "ImgRoot": "https://m.media-amazon.com/images/I/81CxvebthLS._AC_SX355_.jpg",
         "Stock": 10
     },
     {
         "Id":"4",
-        "Name":"Board",
+        "Name":"Motherboard Azus",
         "Price": 300,
         "ImgRoot": "https://m.media-amazon.com/images/I/61mngTZGfiL._AC_SY355_.jpg",
         "Stock": 10
     },
     {
         "Id":"5",
-        "Name":"Monitor",
+        "Name":"Monitor Sangsum",
         "Price": 250,
         "ImgRoot": "https://m.media-amazon.com/images/I/61SQz8S+fEL._AC_SY355_.jpg",
         "Stock": 10
@@ -50,6 +50,7 @@ const articulos = [
     }
 ]
 const loyoutItems = document.getElementById('LoyoutItems');
+const listaCarrito = document.getElementById('items');
 
 
 
@@ -58,6 +59,63 @@ document.addEventListener('DOMContentLoaded', ()=>{
     FillLayoutWidthItems();
 
 });
+
+function AdditemToCar(ID){
+    let item = articulos.find(item =>{
+        if(item.Id === ID) return item;
+    });
+
+    let count = listaCarrito.childElementCount;
+
+    const tr = document.createElement('tr');
+    tr.id = item.Id;
+
+    const number = document.createElement('td');
+    number.innerText = count + 1;
+    tr.appendChild(number);
+
+    const name = document.createElement('td');
+    name.innerText = item.Name;
+    tr.appendChild(name);
+
+    const cant = document.createElement('td');
+    cant.innerText = 1;
+    tr.appendChild(cant);
+
+    const buttons = document.createElement('td');
+
+    const plus = document.createElement('button');
+    plus.className = 'btn btn-info btn-sm';
+    plus.innerText = '+';
+    plus.addEventListener('click', (e)=>{
+        AddItems(item.Id);
+    });
+    buttons.appendChild(plus);
+
+    const minus = document.createElement('button');
+    minus.className = 'btn btn-danger btn-sm';
+    minus.innerText = '-';
+    minus.addEventListener('click', (e)=>{
+        RemoveItem(item.Id);
+    });
+    buttons.appendChild(minus);
+    tr.appendChild(buttons);
+
+    const total = document.createElement('td');
+    total.innerText = item.Price;
+    tr.appendChild(total);
+
+    listaCarrito.appendChild(tr);
+}
+
+function AddItems(Id){
+    console.log(Id);
+
+}
+
+function RemoveItem(Id){
+    console.log(Id);
+}
 
 
 function FillLayoutWidthItems(){
@@ -103,75 +161,35 @@ function FillLayoutWidthItems(){
         p.innerText = `- stock ${item.Stock}`;
         divText.appendChild(p);
 
-        
-
-
-
-
-
-
+        const h5 = document.createElement('h5');
+        h5.className = 'fw-bolder';
+        h5.innerText = item.Name;
+        divText.appendChild(h5);
 
         divCardBody.appendChild(divText);
         card.appendChild(divCardBody);
 
+        const footer = document.createElement('div');
+        footer.className = 'card-footer p-4 pt-0 border-top-0 bg-transparent card-f-ocultar';
 
+
+        const divTextCenter = document.createElement('div');
+        divTextCenter.className = 'text-center';
+
+        const tag = document.createElement('a');
+        tag.className = 'btn btn-outline-dark mt-auto';
        
+        tag.id = item.Id;
+        tag.innerHTML = '<i class="bx bxs-cart-add" ></i> add';
+        tag.addEventListener('click', (e)=>{
+            AdditemToCar(e.target.id);
+        })
 
+        divTextCenter.appendChild(tag);
 
-        card.insertAdjacentHTML = `
-                        <div class="card-body p-4 card-b-margin">
-                            <div class="text-center detail-product ">
-                                <h4>${item.Price} $</h4>
-                                <p>- stock: 99</p>
-                                <h5 class="fw-bolder">Name Product</h5>
-                            </div>
-                        </div>
-        
-        `;
-
-
+        footer.appendChild(divTextCenter);
+        card.appendChild(footer);
         col.appendChild(card);
-
-
-
-        //     <div class="col mb-5">
-        
-        //     <div class="card h-100 seleccion">
-        
-                // <div class="circulo-stock">
-                //     <div class="item-cant">99</div>
-                // </div>
-        
-        //         <img class="card-img-top" src="assets/laptop.jpg" alt="..." />
-               
-        
-        
-                // <div class="card-body p-4 card-b-margin">
-        
-                //     <div class="text-center detail-product ">
-                       
-                //         <h4>10$</h4>
-                //         <p>- stock: 99</p>
-                //         <h5 class="fw-bolder">Name Product</h5>
-                       
-                       
-                //     </div>
-                // </div>
-        
-        //         <div class="card-footer p-4 pt-0 border-top-0 bg-transparent card-f-ocultar">
-        //             <div class="text-center">
-        //                 <a class="btn btn-outline-dark mt-auto" href="#"><i class='bx bxs-cart-add' ></i> add</a>
-        //             </div>
-        //         </div>
-        //     </div>
-        // </div>
-        
-        
-        
-        
-        
-        
-        
         loyoutItems.appendChild(col);
         
         });
